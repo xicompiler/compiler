07cfa79adb0682f52f25760effaf18b1fa12e654
Benjamin Shen <bfs45@cornell.edu>
2022-02-02 20:58:26 -0500

    fix mli warning

--- parsing/ast.ml
+++ parsing/ast.ml
@@ -1,3 +1,1 @@
 type expr = unit
-(** An [expr] is an expression representing a node of the Abstract
-    Syntax Tree of a Xi expression *)
--- parsing/type.ml
+++ parsing/type.ml
@@ -1,10 +1,7 @@
-(** A [primitive] is the type of a primitive value in Xi: either an
-    integer or a boolean *)
 type nonrec primitive =
   | Int
   | Bool
 
-(** A type in Xi is either a primitive type or an array of a type *)
 type t =
   | Primitive of primitive
   | Array of t
