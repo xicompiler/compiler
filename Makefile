071c11d18de93003890d78b673075f909517eac9
vmj5 <vmj5@cornell.edu>
2022-02-06 20:25:46 -0500

    tests

--- Makefile
+++ Makefile
@@ -1,12 +1,24 @@
+.PHONY:test
+
 build:
 	dune build
 
 lex:
 	dune exec ./bin/main.exe
 
+test:
+	dune exec ./tests_unit/main.exe
+
 clean:
 	dune clean
 	rm -rf xic *.lexed *.zip
+
+bisect: bisect-clean
+	dune exec --instrument-with bisect_ppx --force ./tests_unit/main.exe
+	bisect-ppx-report html
+
+bisect-clean:
+	rm -rf _coverage bisect*.coverage
 
 zip:
 	make clean
--- tests_unit/main.ml
+++ tests_unit/main.ml
@@ -1,7 +1,16 @@
 open OUnit2
 open Parsing
 
+
+let make_parsing_test test_name input expected = 
+  test_name >:: (fun _ -> assert_equal (Lexer.lex_string input) expected)
+
+let make_parsing_test_ok test_name input expected_tokens =
+  make_parsing_test test_name input (List.map Result.ok expected_tokens)
+
 let parsing_tests = [
+  make_parsing_test_ok "test string hi" "\"hi\"" [Parser.STRING "hi"];
+  make_parsing_test_ok "test unicode" "\"\\x{190}\"" [Parser.STRING "Ɛ"];
 ]
 
 let suite =
