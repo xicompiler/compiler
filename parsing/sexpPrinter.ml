77ce28ed22f595b880465bea709b79a28e4fff81
Zak Kent <zak33@cornell.edu>
2022-02-16 12:03:25 -0500

    Fixed array types

--- parsing/sexpPrinter.ml
+++ parsing/sexpPrinter.ml
@@ -1,31 +1,20 @@
 open Core
 
-let print_iter ~n out s =
-  for i = 1 to n do
-    Printf.fprintf out "%s" s
-  done
-
-(** [print_lvl out lvl exp] prints the s-expression *)
-let rec print_lvl out lvl sexp =
-  match sexp with
+(** [print_lvl out exp] prints the s-expression [exp] to out channel
+    [out] *)
+let rec print out = function
   | Sexp.Atom e -> Printf.fprintf out "%s" e
   | Sexp.List es ->
-      if lvl > 1 then Printf.fprintf out "\n";
-      print_iter ~n:lvl out "  ";
       Printf.fprintf out "(";
-      print_list out (succ lvl) es;
+      print_list out es;
       Printf.fprintf out ")"
 
 (** [print_list out es] prints the list of s-expressions [es] to out
     channel [out] *)
-and print_list out lvl es =
-  print_iter ~n:lvl out "  ";
-  match es with
+and print_list out = function
   | [] -> ()
-  | [ e ] -> print_lvl out lvl e
+  | [ e ] -> print out e
   | e :: es ->
-      print_lvl out lvl e;
+      print out e;
       Printf.fprintf out " ";
-      print_list out lvl es
-
-let print out = print_lvl out 1
+      print_list out es
