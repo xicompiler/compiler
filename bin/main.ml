5e35e729f0a1634e025824861bb845f4e6044e44
Zak Kent <zak33@cornell.edu>
2022-02-17 18:35:54 -0500

    fixed build error

--- bin/main.ml
+++ bin/main.ml
@@ -85,14 +85,14 @@ let try_sys_iter ~f lst =
 (** [lex_diagonistic ()] writes the lexer output to a diagnostic file. *)
 let lex_diagnostic () =
   try_sys_iter
-    ~f:(iter_file LexerDebug.lex_to_file ".lexed")
+    ~f:(iter_file Lexer.Diagnostic.lex_to_file ".lexed")
     !input_files
 
 (** [parse_diagonistic ()] writes the parser output to a diagnostic
     file. *)
 let parse_diagnostic () =
   try_sys_iter
-    ~f:(iter_file ParserDebug.parse_to_file ".parsed")
+    ~f:(iter_file Parser.Diagnostic.parse_to_file ".parsed")
     !input_files
 
 (** [filter_valid_files f] returns the files in [f] that have a valid Xi
