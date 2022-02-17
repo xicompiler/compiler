23c4ab2b7d1ff16dd277d4cd8a7b280c038f8c58
Zak Kent <zak33@cornell.edu>
2022-02-17 18:05:33 -0500

    ocamlformat

--- bin/main.ml
+++ bin/main.ml
@@ -4,21 +4,30 @@ open Frontend
 exception FileNotFoundError
 
 let usage_msg = "Usage: xic [options] <source files>"
+
 let input_files = ref []
+
 let output_path = ref ""
+
 let src_path = ref ""
+
 let to_lex = ref false
+
 let to_parse = ref false
+
 let display_help = ref false
 
 (** [try_iter e] evaluates [e] and is [()] regardless of whether the
     evaluation of [e] raises an exception. *)
-let try_iter e = try e with _ -> ()
+let try_iter e =
+  try e with
+  | _ -> ()
 
 (** [file_path ~path ~file] is [path/file] if path is non-empty and is
     [file] otherwise *)
 let file_path ~path ~file =
-  if String.is_empty path then file else Printf.sprintf "%s/%s" path file
+  if String.is_empty path then file
+  else Printf.sprintf "%s/%s" path file
 
 (** [iter_file f \[extension\] input_file] performs function f on a
     given [input_file] to the previously specified (or default root
@@ -26,10 +35,14 @@ let file_path ~path ~file =
     as [input_file] but with a [ext] extension. *)
 let iter_file f ext input_file =
   let output_file = Filename.chop_extension input_file ^ ext in
-  let output_file_path = file_path ~path:!output_path ~file:output_file in
+  let output_file_path =
+    file_path ~path:!output_path ~file:output_file
+  in
   let output_file_dir = Filename.dirname output_file_path in
   try_iter (Core.Unix.mkdir_p output_file_dir);
-  f ~src:(file_path ~path:!src_path ~file:input_file) ~dst:output_file_path
+  f
+    ~src:(file_path ~path:!src_path ~file:input_file)
+    ~dst:output_file_path
 
 let speclist =
   [
@@ -40,7 +53,9 @@ let speclist =
       Arg.Set_string src_path,
       "Specify where to find input source files." );
     ("--lex", Arg.Set to_lex, "Generate output from lexical analysis.");
-    ("--parse", Arg.Set to_parse, "Generate output from syntactic analysis.");
+    ( "--parse",
+      Arg.Set to_parse,
+      "Generate output from syntactic analysis." );
     ("--help", Arg.Set display_help, "Print a synopsis of options.");
     ("-help", Arg.Set display_help, "Print a synopsis of options.");
   ]
@@ -52,7 +67,8 @@ let print_help () = print_string (Arg.usage_string speclist usage_msg)
     line arguments, mutating [input_files] to reflect the parsed files. *)
 let try_get_files () =
   let file_acc f = input_files := f :: !input_files in
-  try Arg.parse speclist file_acc usage_msg with _ -> print_help ()
+  try Arg.parse speclist file_acc usage_msg with
+  | _ -> print_help ()
 
 (** [parse_command ()] parses the command and command line arguments. *)
 let parse_command () =
@@ -63,16 +79,21 @@ let parse_command () =
     [Sys_error] is raised during evaluation, the error message is
     printed to stdout. *)
 let try_sys_iter ~f lst =
-  try List.iter ~f lst with Sys_error err -> print_endline err
+  try List.iter ~f lst with
+  | Sys_error err -> print_endline err
 
 (** [lex_diagonistic ()] writes the lexer output to a diagnostic file. *)
 let lex_diagnostic () =
-  try_sys_iter ~f:(iter_file LexerDebug.lex_to_file ".lexed") !input_files
+  try_sys_iter
+    ~f:(iter_file LexerDebug.lex_to_file ".lexed")
+    !input_files
 
 (** [parse_diagonistic ()] writes the parser output to a diagnostic
     file. *)
 let parse_diagnostic () =
-  try_sys_iter ~f:(iter_file ParserDebug.parse_to_file ".parsed") !input_files
+  try_sys_iter
+    ~f:(iter_file ParserDebug.parse_to_file ".parsed")
+    !input_files
 
 (** [filter_valid_files f] returns the files in [f] that have a valid Xi
     extension and exist in the file system. *)
@@ -84,9 +105,12 @@ let filter_valid_files files =
     else
       let ext = Caml.Filename.extension file in
       match ext with
-      | ".xi" | ".ixi" -> true
+      | ".xi"
+      | ".ixi" ->
+          true
       | _ ->
-          printf "Warning: %s is not a .xi or .ixi file - ignored\n" file;
+          printf "Warning: %s is not a .xi or .ixi file - ignored\n"
+            file;
           false
   in
   List.filter files ~f
